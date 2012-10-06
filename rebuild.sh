9f35629b743071f4d24ad73f969827886e3533d1
Milen Rangelov <gat3way@gat3way.eu>
2012-10-06 14:24:45 +0300

    Properly recreate libtool-related stuff

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+rm aclocal.m4
+rm ltmain.sh
+#rm configure
 autoscan && autoconf && aclocal && autoreconf -vi && automake --add-missing && autoconf
 #aclocal && autoheader && automake && autoconf
