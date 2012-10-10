c332b633e6e2abd8296e883ff044667f9f891141
Milen Rangelov <gat3way@gat3way.eu>
2012-10-10 17:33:44 +0300

    Finally fix that autoconf/libtool crap

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-rm aclocal.m4
-rm ltmain.sh
 #rm configure
-autoscan ; autoconf ; aclocal ;libtoolize --force ; autoreconf -vi ; automake --add-missing ; autoconf
+#autoscan ; autoconf ; aclocal ;
+libtoolize --force ; autoreconf -vi ; automake --add-missing ; autoconf
 #aclocal ; autoheader ; automake ; autoconf
