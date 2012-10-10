7e2701ae88bacb717ee7753bc8ae8c8f67e7b105
Milen Rangelov <gat3way@gat3way.eu>
2012-10-11 00:01:56 +0300

    rebuild.sh update - remove ANY traces of libtool version ANYWHERE

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 #rm configure
 #autoscan ; autoconf ; aclocal ;
+find m4/ |xargs --replace=as unlink "as" 2>/dev/null
+if [ -e ltmain.sh ];then unlink ltmain.sh; fi
+rm -f autom4te.cache/*
+rm -f libtool
+rm -f configure.status
+rm -f config.status
+rm -f configure
+rm -f src/*.lo
+rm -f src/*.la
+rm -f src/.libs/*
 libtoolize --force ; autoreconf -vi ; automake --add-missing ; autoconf
 #aclocal ; autoheader ; automake ; autoconf
