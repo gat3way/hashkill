2eec5aad328ce3046fb4d0e9c2549262ad23eb6a
Milen Rangelov <gat3way@gat3way.eu>
2012-10-11 00:50:40 +0300

    Commiting first self-tests

---
+++ tests/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ERRORS=0
+TESTS=0
+PARAM="$1"
+echo -e "Performing tests...\n"
+
+while IFS='|' read -r TEST_TARGET TEST_NEEDLE TEST_COMMAND
+do
+tput sgr0
+COMMAND="$TEST_COMMAND $PARAM"
+echo -en "Testing $TEST_TARGET";
+$COMMAND 2>output >&2 > runtest.sh </dev/console;
+RESULT=`grep "$TEST_NEEDLE" output`;
+TESTS=`expr $TESTS + 1`
+if [ ! -z "$RESULT" ]; then
+    echo -e '\E[32;40m Success'
+    tput sgr0
+else
+    ERRORS=`expr $ERRORS + 1`
+    echo -e '\E[31;40m FAILURE!'
+    tput sgr0
+fi
+done < test.dat
+
+echo -en "\n\n$TESTS tests performed, $ERRORS errors found.\n\n"
